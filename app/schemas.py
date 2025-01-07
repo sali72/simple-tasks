7207c4341e7d4169798daf66bd6115bea043e789
sali72 <sahashemi072@gmail.com>
2025-01-07 13:57:54 +0330

    feat: add schemas.py

---
+++ app/schemas.py
@@ -0,0 +1,27 @@
+from datetime import datetime
+
+from pydantic import BaseModel, Extra, Field
+
+
+class BaseModelConfigured(BaseModel):
+    """
+    Adds configuration to base model to use for all schemas.
+
+    Configurations:
+    forbid extra fields
+    """
+
+    class Config:
+        extra = Extra.forbid
+
+
+class TaskCreationSchema(BaseModelConfigured):
+    title: str = Field(..., title="The task title")
+    is_completed: bool = Field(False, title="The task completion status")
+    description: str = Field(None, title="The task description")
+
+
+class ResponseSchema(BaseModelConfigured):
+    message: str = Field(None, example="success")
+    data: dict = Field(None, example={"data": "Your requested data"})
+    timestamp: datetime = Field(datetime.now(), example="2024-02-16T14:05:09.252968")
