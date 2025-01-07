0391d5d7580f167436e78193251d6fd75d0ecb0c
sali72 <sahashemi072@gmail.com>
2025-01-07 18:14:19 +0330

    docs: add docs for the routes

--- app/main.py
+++ app/main.py
@@ -1,12 +1,27 @@
-from commons.exception_handlers import base_exception_handler, http_exception_handler
 from fastapi import FastAPI, HTTPException
 
 from app.database import Base, engine
 from app.routers.tasks import tasks_router
+from commons.exception_handlers import base_exception_handler, http_exception_handler
 
 Base.metadata.create_all(engine)
 
-app = FastAPI()
+app = FastAPI(
+    title="Simple Tasks API",
+    description=(
+        "This API allows you to manage tasks, including creating, updating, "
+        "retrieving, and deleting tasks. It is designed to be simple and easy to use."
+    ),
+    version="0.1.0",
+    contact={
+        "name": "Seyed Ali Hashemi",
+        "email": "sahashemi072@gmail.com",
+    },
+    license_info={
+        "name": "MIT License",
+        "url": "https://opensource.org/licenses/MIT",
+    },
+)
 
 app.include_router(tasks_router)
 
--- app/routers/tasks.py
+++ app/routers/tasks.py
@@ -8,6 +8,19 @@ tasks_router = APIRouter(prefix="/tasks", tags=["Tasks"])
 
 @tasks_router.post("", response_model=ResponseSchema)
 async def create_task_route(task_schema: TaskCreationSchema):
+    """Create a new task.
+
+    Args:
+        task_schema (TaskCreationSchema): The schema containing the task details.
+            - title (str): The task title.
+            - is_completed (bool): The task completion status. Defaults to False.
+            - description (str): The task description. Optional.
+
+    Returns:
+        ResponseSchema: A response containing the ID of the created task and a success message.
+            - message (str): A success message.
+            - data (dict): Contains the ID of the created task.
+    """
     _id = await TasksController.create_task(task_schema)
 
     message = "Task created successfully"
@@ -17,6 +30,20 @@ async def create_task_route(task_schema: TaskCreationSchema):
 
 @tasks_router.get("/{task_id}", response_model=ResponseSchema)
 async def get_task_route(task_id: int):
+    """Retrieve a task by its ID.
+
+    Args:
+        task_id (int): The ID of the task to retrieve.
+
+    Returns:
+        ResponseSchema: A response containing the task details and a success message.
+            - message (str): A success message.
+            - data (dict): Contains the task details.
+                - title (str): The task title.
+                - is_completed (bool): The task completion status.
+                - description (str): The task description.
+                - created_at (datetime): The timestamp when the task was created.
+    """
     task = await TasksController.get_task(task_id)
 
     message = "Task retrieved successfully"
@@ -26,6 +53,18 @@ async def get_task_route(task_id: int):
 
 @tasks_router.get("", response_model=ResponseSchema)
 async def get_tasks_route():
+    """Retrieve all tasks.
+
+    Returns:
+        ResponseSchema: A response containing a list of all tasks and a success message.
+            - message (str): A success message.
+            - data (dict): Contains a list of tasks.
+                - Each task includes:
+                    - title (str): The task title.
+                    - is_completed (bool): The task completion status.
+                    - description (str): The task description.
+                    - created_at (datetime): The timestamp when the task was created.
+    """
     tasks = await TasksController.get_tasks()
 
     message = "Tasks retrieved successfully"
@@ -35,6 +74,20 @@ async def get_tasks_route():
 
 @tasks_router.put("/{task_id}", response_model=ResponseSchema)
 async def update_task_route(task_id: int, task_schema: TaskUpdateSchema):
+    """Update a task by its ID.
+
+    Args:
+        task_id (int): The ID of the task to update.
+        task_schema (TaskUpdateSchema): The schema containing the updated task details.
+            - title (Optional[str]): The task title. Optional.
+            - is_completed (Optional[bool]): The task completion status. Optional.
+            - description (Optional[str]): The task description. Optional.
+
+    Returns:
+        ResponseSchema: A response containing the updated task details and a success message.
+            - message (str): A success message.
+            - data (dict): Contains the updated task details.
+    """
     task = await TasksController.update_task(task_id, task_schema)
 
     message = "Task updated successfully"
@@ -44,6 +97,15 @@ async def update_task_route(task_id: int, task_schema: TaskUpdateSchema):
 
 @tasks_router.delete("/{task_id}", response_model=ResponseSchema)
 async def delete_task_route(task_id: int):
+    """Delete a task by its ID.
+
+    Args:
+        task_id (int): The ID of the task to delete.
+
+    Returns:
+        ResponseSchema: A response containing a success message.
+            - message (str): A success message.
+    """
     await TasksController.delete_task(task_id)
 
     message = "Task deleted successfully"
