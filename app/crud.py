dd2692919dbfc1cfd844568a90924c48ea224df2
sali72 <sahashemi072@gmail.com>
2025-01-07 16:28:03 +0330

    feat: add crud methods

--- app/crud.py
+++ app/crud.py
@@ -10,3 +10,23 @@ class TasksCRUD:
         db.commit()
         db.refresh(task)
         return task
+    
+    @classmethod
+    async def get_task(cls, task_id: int) -> Task:
+        return db.query(Task).filter(Task.id == task_id).first()
+    
+    @classmethod
+    async def get_tasks(cls) -> list[Task]:
+        return db.query(Task).all()
+    
+    @classmethod
+    async def update_task(cls, task_id: int, task: Task) -> Task:
+        db.query(Task).filter(Task.id == task_id).update(task.dict())
+        db.commit()
+        return task
+    
+    @classmethod
+    async def delete_task(cls, task_id: int) -> None:
+        db.query(Task).filter(Task.id == task_id).delete()
+        db.commit()
+        return None
